6fa9ccb4d0486f84b01678aeaabe9e8891c251b6
FuHsinyu <x.fu1@uu.nl>
2026-03-23 16:01:07 +0100

    yda-6699: add auto config to skip wizard setup

--- docker/images/matomo/matomo-init.sh
+++ docker/images/matomo/matomo-init.sh
@@ -50,17 +50,21 @@ then before_update "Setting up Apache vhost"
      perl -pi -e '$servername=$ENV{MATOMO_HOST}; s/SERVERNAME/$servername/' /etc/apache2/sites-available/001-matomo.conf
      before_update "Initializing Matomo."
      cd /var/www/html/matomo
-     ./console matomo:install --no-interaction \
-	     --db-username="$MATOMO_DATABASE_USERNAME" \
-	     --db-pass="MATOMO_DATABASE_PASSWORD" \
-             --db-host="$MATOMO_DATABASE_HOST" \
-	     --db-port=3306 \
-	     --db-name="$MATOMO_DATABASE_DBNAME" \
-             --first-site-name="$MATOMO_FIRST_SITE_NAME" \
-	     --first-site-url="$MATOMO_FIRST_SITE_URL" \
-             --first-user="$MATOMO_FIRST_USER_NAME" \
-             --first-user-email="$MATOMO_FIRST_USER_EMAIL" \
-             --first-user-pass="$MATOMO_FIRST_USER_PASSWORD"
+     sudo -u www-data php console matomo:install --no-interaction --force -vvv \
+       --db-username="$MATOMO_DATABASE_USERNAME" \
+       --db-pass="$MATOMO_DATABASE_PASSWORD" \
+       --db-host="$MATOMO_DATABASE_HOST" \
+       --db-port=3306 \
+       --db-name="$MATOMO_DATABASE_DBNAME" \
+       --db-prefix="${MATOMO_DATABASE_TABLES_PREFIX:-matomo_}" \
+       --db-adapter="${MATOMO_DATABASE_ADAPTER:-mysqli}" \
+       --db-collation=utf8mb4_general_ci \
+       --db-charset=utf8mb4 \
+       --first-site-name="$MATOMO_FIRST_SITE_NAME" \
+       --first-site-url="$MATOMO_FIRST_SITE_URL" \
+       --first-user="$MATOMO_FIRST_USER_NAME" \
+       --first-user-email="$MATOMO_FIRST_USER_EMAIL" \
+       --first-user-pass="$MATOMO_FIRST_USER_PASSWORD"
      crudini --set /var/www/html/matomo/config/config.ini.php General force_ssl 1
      crudini --set /var/www/html/matomo/config/config.ini.php General assume_secure_protocol 1
      crudini --set /var/www/html/matomo/config/config.ini.php General proxy_client_headers[] HTTP_X_FORWARDED_FOR
